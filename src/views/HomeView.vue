67b55a92d5e6a824067e5ede40aae33f195991a9
zuo <zuojinxiang@hyperchain.cn>
2023-06-21 11:01:12 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div>the project of account</div>
+    <div>the project of account</div>
+    <div>the project of account</div>
+    <TheWelcome />
+  </main>
+</template>
